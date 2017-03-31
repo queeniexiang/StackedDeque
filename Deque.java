9765bad0d32916edd362ef61f20a06322d96f6e8
JamesSmith1202 <jsmith@stuy.edu>
2017-03-31 10:14:00 -0400

    added our deque interface

---
+++ Deque.java
@@ -0,0 +1,31 @@
+public interface Deque<t>{
+
+    //   public boolean add(t value);
+
+    // public void addFirst(t value);
+
+    //    public boolean contains(t value);
+
+    // public Iterator<t> iterator();
+
+    //    public Iterator<t> descendingIterator();
+
+    //    public t peek();
+
+    //    public t peekLast();
+
+    //    public t getFirst();
+
+    //  public t getLast();
+
+    //  public t remove();
+
+    //  public boolean remove(Object o);
+
+    //  public t removeLast();
+
+    //  public int size();
+
+    //  public boolean isEmpty();
+
+}
